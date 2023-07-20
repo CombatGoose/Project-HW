96e5f439ce72b320f9eef9d1f54d6321f5d8e150
CombatGoose <glebmikhov@gmail.com>
2023-07-20 20:13:27 +0300

    Try To fix bug

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = { 
-    port: 4000,
-    mongoKey: 'mongodb+srv://admin:admin@cluster0.hhy4ah8.mongodb.net/?retryWrites=true&w=majority',
-    corsOptions: { 
-        origin:'*', 
-        credentials:true,            
-        optionSuccessStatus:200
-    }
-}
--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const formEl = {
+const body = document.querySelector('.products-block')
+const buttonChange = document.querySelector('.buttonChange')
+const paginationElements = document.querySelectorAll('.pagination-elements')
+const paginationPrev = document.querySelector('#paginationPrevButton')
+const paginationNext = document.querySelector('#paginationNextButton')
+
+const formCreate = {
     name: document.querySelector('#formName'),
     description: document.querySelector('#formDescription'),
     quantity: document.querySelector('#formQuantity'),
@@ -6,16 +12,98 @@ const formEl = {
     btnCreate: document.querySelector('.formButton')
 }
 
-formEl.btnCreate.addEventListener('click', () => {
-    const formData = {
-        name: formEl.name.value,
-        description: formEl.description.value,
-        quantity: formEl.quantity.value,
-        price: formEl.price.value
+const formChange = {
+    id: document.querySelector('#takeId'),
+    newName: document.querySelector('#changeName'),
+    button: document.querySelector('.formButtonChange')
+}
+
+let products = []
+let countProduct
+
+const getProducts = () => {
+    axios.get("http://localhost:4000/products/get-all")
+        .then((res) => {
+            products = [
+                ...res.data
+            ]
+            countProduct = res.data.length
+            renderProducts()
+        })
+}
+
+const renderProducts = () => { 
+    body.innerHTML = ""
+    products.forEach(productItem => { 
+        body.innerHTML += `
+        <div element-id="${productItem.id}" class="block">
+            <p>${productItem.name}</p>
+            <p>${productItem.quantity}</p>
+            <p>${productItem.price}</p>
+            <p>${productItem.description}</p>
+            <button element-id="${productItem.id}" class="formButtonDelete">Delete</button>
+        </div>
+        `
+    })
+
+    const btnsDelete = document.querySelectorAll(".formButtonDelete")
+
+    btnsDelete.forEach(btnEl => {
+        btnEl.addEventListener("click", () => {
+            const productId = btnEl.getAttribute("element-id")
+            axios.delete(`http://localhost:4000/products/delete?id=${productId}`)
+                .then((res) => {
+                    getProducts()
+                })
+        })
+    })
+
+}
+
+const currentProductId = "2qQTszjI"
+
+axios.get(`http://localhost:4000/products/get-item?id=${currentProductId}`)
+    .then(res => {
+        console.log(res.data)
+    })
+
+getProducts()
+
+const limit = 10
+const page = 1
+    
+    axios.get(`http://localhost:4000/products/get?page=${page}&limit=${limit}`) 
+        .then(res => {
+            console.log(res.data)
+        })
+
+        axios.get(`http://localhost:4000/products/count`)
+            .then(res => {
+                console.log(res.data)
+            })
+
+formCreate.btnCreate.addEventListener('click', () => { 
+    const formData = { 
+        name: formCreate.name.value,
+        quantity: formCreate.quantity.value,
+        price: formCreate.price.value,
+        description: formCreate.description.value
+    }
+    
+    axios.post('http://localhost:4000/products/create', {...formData})
+        .then(res => { 
+            getProducts()
+        })
+})
+
+formChange.button.addEventListener('click', () => {
+    const formChangeData = {
+        id: formChange.id.value,
+        name: formChange.newName.value
     }
 
-    axios.post('http://localhost:4000/products/create', {...formData})
-        .then(res => {
-            console.log(res)
-    })
+        axios.put(`http://localhost:4000/products/change-price?id=${formChangeData.id}`)
+            .then(res => {
+                console.log(res.data)
+            })
 })
